6121dffe4d98c7cf13c6297a7a72b1d6b8e851c5
Aman Vishwakarma <vishamanofficial@gmail.com>
2024-08-21 22:33:51 +0530

    dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 let menu_close_btn = document.querySelector(".menu-close-btn");
 let menu_open_btn = document.querySelector(".menu-open");
 let mobile_nav_box = document.querySelector(".side-menu");
+let menu_items = document.querySelectorAll(".side-menu ul li a");
+
+// Function to close the menu
+function closeMenu() {
+    mobile_nav_box.classList.remove("open");
+}
+
+// Open menu when the menu button is clicked
 menu_open_btn.addEventListener('click', function() {
     mobile_nav_box.classList.add("open");
-    
-})
-menu_close_btn.addEventListener('click' , function(){
-    mobile_nav_box.classList.remove("open");
-})
+});
 
+// Close menu when the close button is clicked
+menu_close_btn.addEventListener('click', closeMenu);
 
+// Close menu when any menu item is clicked
+menu_items.forEach(function(item) {
+    item.addEventListener('click', closeMenu);
+});
